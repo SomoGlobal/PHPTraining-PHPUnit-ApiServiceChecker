7e143d4909e9650ee212e6bc82a11754555784ec
Piers Roberts <piers.roberts@somoglobal.com>
2016-05-26 15:50:51 +0100

    resolved conflicts

--- src/EndpointDefinition/EndpointDefinitionServiceInterface.php
+++ src/EndpointDefinition/EndpointDefinitionServiceInterface.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD:src/EndpointDefinitionServiceInterface.php
-namespace Training\PHPUnit\ApiServiceChecker\Alert;
-=======
 namespace Training\PHPUnit\ApiServiceChecker\EndpointDefinition;
->>>>>>> c61c599d45a062c5bce131a23260cbf18cc8d515:src/EndpointDefinition/EndpointDefinitionServiceInterface.php
 
 interface EndpointDefinitionServiceInterface
 {
