a18d27d9bdceb28016e0a0619c482077bb926d92
Piers Roberts <piers.roberts@somoglobal.com>
2016-05-26 16:57:53 +0100

    Fixed my tests to use the interfaces

--- test/ApiCheckerTest.php
+++ test/ApiCheckerTest.php
@@ -1,13 +1,13 @@
 <?php
 use Training\PHPUnit\ApiServiceChecker\Checker\ApiChecker;
 use Training\PHPUnit\ApiServiceChecker\EndpointDefinition\EndpointDefinitionServiceInterface;
-use Training\PHPUnit\ApiServiceChecker\EndpointChecker;
+use Training\PHPUnit\ApiServiceChecker\EndpointChecker\EndpointCheckerInterface;
 use Training\PHPUnit\ApiServiceChecker\Alert\AlertServiceInterface;
 use Training\PHPUnit\ApiServiceChecker\Alert\Alert;
 class ApiCheckerTest extends \PHPUnit_Framework_TestCase {
 
     function setUp(){
-        $endpointCheckerService = $this->getMockBuilder(EndpointCheckerService::class)->getMock();
+        $endpointCheckerService = $this->getMockBuilder(EndpointCheckerInterface::class)->getMock();
         $endpointDefinitionService = $this->getMockBuilder(EndpointDefinitionServiceInterface::class)->getMock();
         $alertService = $this->getMockBuilder(AlertServiceInterface::class)->getMock();
 
