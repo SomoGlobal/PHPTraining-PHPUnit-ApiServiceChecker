3444e50f4e661be1274a30b79cac29a4db27d076
Glenn Flanagan <glenn.flanagan@somoglobal.com>
2016-05-26 16:26:24 +0100

    EndPointCheckerTest basic test

---
+++ test/EndPointChecker/EndPointCheckerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Training\PHPUnit\ApiServiceChecker\Test;
+
+
+class EndPointCheckerTest extends \PHPUnit_Framework_TestCase {
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
